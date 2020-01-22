65260826496e58606c5847ee9d4200e1d2b3f829
Frank Lam <ryzingsun11@yahoo.com>
2020-01-22 17:23:04 +0800

    Define initialization interface for LarvataMine::RestClient

---
+++ lib/larvata_mine/rest_client.rb
@@ -0,0 +1,20 @@
+require 'http'
+
+module LarvataMine
+  class RestClient
+    attr_reader :api_key, :base_url
+
+    def initialize(**options)
+      verify_config(**options)
+      @client = HTTP.headers("X-Redmine-API-Key" => api_key)
+    end
+
+    private
+
+    def verify_config(**options)
+      @api_key = options.fetch(:api_key) { ENV["REDMINE_API_KEY"] }
+      @base_url = options.fetch(:base_url) { ENV["REDMINE_BASE_URL"] }
+      raise ArgumentError, "Missing API key and/or base URL" unless api_key && base_url
+    end
+  end
+end
---
+++ spec/rest_client_spec.rb
@@ -0,0 +1,19 @@
+require "larvata_mine/rest_client"
+require "webmock/rspec"
+
+RSpec.describe LarvataMine::RestClient do
+  context "without a Redmine API key" do
+    it "raises ArgumentError" do
+      base_url = "https://fake.com"
+      expect { LarvataMine::RestClient.new(base_url: base_url) }.to raise_error ArgumentError
+    end
+  end
+
+  context "without a Redmine base URL" do
+    it "raises ArgumentError" do
+      api_key = "fake"
+      expect { LarvataMine::RestClient.new(api_key: api_key) }.to raise_error ArgumentError
+    end
+  end
+
+end
