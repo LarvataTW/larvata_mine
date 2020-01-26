7e7d98b873393090ff9ba40fe158cfba2a496f86
Frank Lam <ryzingsun11@yahoo.com>
2020-01-26 17:00:43 +0800

    Test Redmine response with invalid API key

--- spec/rest_client_spec.rb
+++ spec/rest_client_spec.rb
@@ -1,5 +1,6 @@
 require "larvata_mine/rest_client"
 require "webmock/rspec"
+require "dotenv"
 
 RSpec.describe LarvataMine::RestClient do
   context "without a Redmine API key" do
@@ -13,6 +14,21 @@ RSpec.describe LarvataMine::RestClient do
     it "raises ArgumentError" do
       api_key = "fake"
       expect { LarvataMine::RestClient.new(api_key: api_key) }.to raise_error ArgumentError
+    end
+  end
+
+  context "with an invalid Redmine API key" do
+    it "responds with a redirect to login" do
+      Dotenv.load
+      api_key = "fake"
+      client = LarvataMine::RestClient.new(api_key: api_key)
+
+      stub_request(:get, "#{client.base_url}/issues")
+        .with(headers: { "X-Redmine-API-Key" => api_key })
+        .to_return(status: 302)
+      response = client.all_issues
+
+      expect(response.code).to eq 302
     end
   end
 
