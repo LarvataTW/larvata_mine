9de5dccceae5d1a4a30d8e5ff580bfe233ab212d
Frank Lam <ryzingsun11@yahoo.com>
2020-02-14 11:39:28 +0800

    Set global timeout via initialization options or envars

--- lib/larvata_mine/rest_client.rb
+++ lib/larvata_mine/rest_client.rb
@@ -2,11 +2,11 @@ require 'http'
 
 module LarvataMine
   class RestClient
-    attr_reader :api_key, :base_url
+    attr_reader :api_key, :base_url, :timeout
 
     def initialize(**options)
       verify_config(**options)
-      @client = HTTP.headers("X-Redmine-API-Key" => api_key)
+      @client = HTTP.headers("X-Redmine-API-Key" => api_key).timeout(timeout)
     end
 
     def all_issues
@@ -18,6 +18,7 @@ module LarvataMine
     def verify_config(**options)
       @api_key = options.fetch(:api_key) { ENV["REDMINE_API_KEY"] }
       @base_url = options.fetch(:base_url) { ENV["REDMINE_BASE_URL"] }
+      @timeout = options.fetch(:timeout) { ENV["REQUEST_TIMEOUT"].to_i }
       raise ArgumentError, "Missing API key and/or base URL" unless api_key && base_url
     end
   end
