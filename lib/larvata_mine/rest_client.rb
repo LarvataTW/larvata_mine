dd3da1c2a423243e58cc25214f8d95e329a92c93
Frank Lam <ryzingsun11@yahoo.com>
2020-02-17 15:09:25 +0800

    Namespace REQUEST_TIMEOUT envar

--- lib/larvata_mine/rest_client.rb
+++ lib/larvata_mine/rest_client.rb
@@ -24,7 +24,7 @@ module LarvataMine
     def verify_config(**options)
       @api_key = options.fetch(:api_key) { ENV["REDMINE_API_KEY"] }
       @base_url = options.fetch(:base_url) { ENV["REDMINE_BASE_URL"] }
-      @timeout = options.fetch(:timeout) { ENV["REQUEST_TIMEOUT"].to_i }
+      @timeout = options.fetch(:timeout) { ENV["REDMINE_REQUEST_TIMEOUT"].to_i }
       raise ArgumentError, "Missing API key and/or base URL" unless api_key && base_url
     end
   end
